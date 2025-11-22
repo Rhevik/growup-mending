55716c74c3ee7a2b686443e8fb28a460a13362af
Matthew Stevens <mstevens@llst.ca>
2025-11-22 13:55:09 -0800

    added some button push logic

--- FlexTaper/GameManager/game_manager.gd
+++ FlexTaper/GameManager/game_manager.gd
@@ -8,3 +8,18 @@ func _ready():
 
 func select_next_challenge():
 	CurrentChallenge = AllChallenges[randi() % AllChallenges.size()] 
+
+func attempt_solution(solution):
+	if(CurrentChallenge.solution.has(solution)):
+		select_next_challenge()
+	else:
+		select_next_challenge()
+		
+func press_kiss():
+	attempt_solution(Challenge.AcceptedSolutions.KISS)
+
+func press_whack():
+	attempt_solution(Challenge.AcceptedSolutions.WHACK)
+
+func press_tape():
+	attempt_solution(Challenge.AcceptedSolutions.TAPE)
