8702f85ddb30ff4e3b2cf1202aa5dae570e7589a
nightmarketgordon <gordon@nightmarket.games>
2025-11-22 16:15:19 -0800

    Competed challenge loop

--- FlexTaper/ChallengeDirector/challenge_director.gd
+++ FlexTaper/ChallengeDirector/challenge_director.gd
@@ -1,23 +1,36 @@
 class_name ChallengeDirector
 extends Node
 
-var test: int
-var introTimer: Timer
-var challengeTimer: Timer
-var introMusic: AudioStreamPlayer
+@export var introTimer: Timer
+@export var challengeTimer: Timer
+@export var introMusic: AudioStreamPlayer
+@export var successMusic: AudioStreamPlayer
+@export var failMusic: AudioStreamPlayer
 var challenge: Challenge
 
-func StartChallenge(newChallenge) -> Challenge.AcceptedSolutions:
+func StartChallenge(newChallenge):
 	challenge = newChallenge
 	await ChallengeIntro()
 	var answer = await ChallengeGame()
-	return answer
+	if(DidPlayerSucceedChallenge(answer)):
+		ChallengeOuttroSuccess();
+	else:
+		ChallengeOuttroFail();
 	
 func ChallengeIntro():
 	introTimer = get_node("IntroTimer")
 	introMusic.play()
 	
 	await introTimer.timeout
+	
+func ChallengeOuttroSuccess():
+	successMusic.Play();
+	await get_tree().create_timer(1.0).timeout
+	
+func ChallengeOuttroFail():
+	failMusic.Play();
+	await get_tree().create_timer(1.0).timeout
+	return 0;
 	
 func ChallengeGame() -> Challenge.AcceptedSolutions:
 	challengeTimer = get_node("ChallengeTimer")
@@ -27,3 +40,10 @@ func ChallengeGame() -> Challenge.AcceptedSolutions:
 		await get_tree().create_timer(0).timeout
 		
 	return Challenge.AcceptedSolutions.NOTHING
+
+func DidPlayerSucceedChallenge(result) -> bool:
+	if (challenge.AcceptedSolutions.find_key(result) != null):
+		return true
+	else:
+		return false
+	
